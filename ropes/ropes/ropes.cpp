0726c39b83a9963b37ff374dd2a4e2d673e04a7a
Jacob B West <jacob.b.west@mines.sdmt.edu>
2021-10-13 21:22:59 -0600

    Added the main and the recursive function

--- ropes/ropes/ropes.cpp
+++ ropes/ropes/ropes.cpp
@@ -2,19 +2,60 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
-int main()
+using namespace std;
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    vector<int> D; //Diameter Array
+    vector<int> S; //Strength Array
+    vector<int> a; //Answer Array
+    
+    if (argc > 1)
+    {
+        ifstream inFile;
+        int diameter = 0;
+        int strength = 0;
+        inFile.open(argv[1]);
+
+        if (!inFile.is_open())
+        {
+            cout << "Failed to open file!" << endl;
+            return -1;
+        }
+
+        while (inFile >> diameter)
+        {
+            inFile >> strength;
+            D.push_back(diameter);
+            S.push_back(strength);
+        }
+
+        inFile.close();
+    }
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int T(int index, int pa, vector<int> D, vector<int> S)
+{
+    if (index == 0 or pa == 0)
+        return 0;
+
+    if (index == 1 and S[1] > pa)
+        return 1;
+
+    if (index > 0 and
+        pa > 0 and
+        S[index] <= pa and
+        S[index - 1] < S[index])
+        return T(index - 1, pa - 1, D, S) + 1;
+
+    if (index > 0 and pa > 0)
+        return max(T(index - 1, pa, D, S), T(index, pa - 1, D, S));
+}
+
