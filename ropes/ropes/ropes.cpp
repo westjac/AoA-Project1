9c0ba830a5c1b6653b355b1a5513dd9c00c7ec8d
Jacob B West <jacob.b.west@mines.sdmt.edu>
2021-10-19 23:37:04 -0600

    Working

--- ropes/ropes/ropes.cpp
+++ ropes/ropes/ropes.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
 
@@ -14,6 +15,8 @@ void LCS_LENGTH(vector<pair<int, int>> A, vector<pair<int, int>> B, vector<vecto
 void GET_LCS(vector<vector<string>> direction, vector<pair<int, int>> A, vector<int>& answer, int x, int y);
 void print2DArray(vector<vector<string>> array);
 void print2DArray(vector<vector<int>> array);
+int Remove_Duplicates(vector<int>& answerArray, vector<pair<int, int>> diameter, vector<pair<int, int>> strength);
+int randNumber();
 
 int main(int argc, char** argv)
 {
@@ -25,7 +28,7 @@ int main(int argc, char** argv)
     answer.resize(Diameter.size());
     
     //Fill vectors
-    if (argc > 1)
+    if (argc == 2)
     {
         ifstream inFile;
         int diameter = 0;
@@ -50,15 +53,59 @@ int main(int argc, char** argv)
         inFile.close();
     }
 
+    //Run Testing if test is written after file in cmd line
+    if (argc == 3)
+    {
+        int numberOfTests = 100;
+        ofstream file;
+        file.open("sortTests.csv", std::ofstream::out | std::ofstream::trunc);
+        file << "Time values are in nanoseconds\n";
+        file << "Test Length, Time (ns), answer length\n";
+
+        for (int i = 2; i < numberOfTests + 1; i++)
+        {
+            //Generate the vectors to sort
+            vector<pair<int, int>> diameter(i);
+            vector<pair<int, int>> strength(i);
+            vector<int> answer; //Answer Array
+            srand(time(NULL));
+            
+            for (int j = 0; j < i; j++)
+            {
+                diameter[j] = make_pair(randNumber(), j);
+                strength[j] = make_pair(randNumber(), j);
+            }
+            vector<pair<int, int>> sortedDiameter = sortDecending(diameter);
+            vector<pair<int, int>> sortedStrength = sortDecending(strength);
+
+            int length = diameter.size();
+            vector<vector<string>> direction(length + 1, vector<string>(length + 1));
+            vector<vector<int>> cost(length + 1, vector<int>(length + 1));
+
+            //Time Tests
+            auto start = chrono::high_resolution_clock::now();
+            LCS_LENGTH(sortedDiameter, sortedStrength, direction, cost);
+            GET_LCS(direction, sortedDiameter, answer, sortedDiameter.size(), sortedDiameter.size());
+            int duplicateCount = Remove_Duplicates(answer, diameter, strength);
+            auto end = chrono::high_resolution_clock::now();
+            auto elapsedTime = end - start;
+
+            file << i << "," << elapsedTime.count() << "," << answer.size() - duplicateCount << "\n";
+        }
+
+        file.close();
+        return 0;
+    }
+
     //Sort each component decending
     vector<pair<int, int>> sortedDiameter = sortDecending(Diameter);
     vector<pair<int, int>> sortedStrength = sortDecending(Strength);
 
-    
+
     //Set direction and cost 2d arrays
     int length = Diameter.size();
-    vector<vector<string>> direction(length+1, vector<string>(length+1));
-    vector<vector<int>> cost(length+1, vector<int>(length+1));
+    vector<vector<string>> direction(length + 1, vector<string>(length + 1));
+    vector<vector<int>> cost(length + 1, vector<int>(length + 1));
 
     //Run LCS
     LCS_LENGTH(sortedDiameter, sortedStrength, direction, cost);
@@ -70,11 +117,16 @@ int main(int argc, char** argv)
     //print2DArray(direction);
     //print2DArray(direction);
 
+    int duplicateCount = Remove_Duplicates(answer, Diameter, Strength);
+
     
     //Print Answer
-    cout << answer.size() << endl;
+    cout << answer.size() - duplicateCount << endl;
     for (int i = 0; i < answer.size(); i++)
-        cout << answer[i] << " -> " << Diameter[answer[i]].first << " : " << Strength[answer[i]].first << endl;
+    {
+        if(answer[i] != -1)
+            cout << answer[i] << " -> " << Diameter[answer[i]].first << " : " << Strength[answer[i]].first << endl;
+    }
 
     return 0;
 }
@@ -134,19 +186,19 @@ void LCS_LENGTH(vector<pair<int, int>> A, vector<pair<int, int>> B, vector<vecto
     {
         for (int y = 1; y <= length; y++)
         {
-            if (A[x-1].second == B[y-1].second)
+            if (A[x-1].second == B[y-1].second) //GRADING: REUSE
             {
-                cost[x][y] = cost[x - 1][y - 1] + 1;
+                cost[x][y] = cost[x - 1][y - 1] + 1; //GRADING: UPDATE
                 direction[x][y] = "UL";
             }
-            else if (cost[x - 1][y] > cost[x][y - 1])
+            else if (cost[x - 1][y] > cost[x][y - 1]) //GRADING: REUSE
             {
-                cost[x][y] = cost[x - 1][y];
+                cost[x][y] = cost[x - 1][y]; //GRADING: UPDATE
                 direction[x][y] = "L";
             }
             else
             {
-                cost[x][y] = cost[x][y - 1];
+                cost[x][y] = cost[x][y - 1]; //GRADING: UPDATE
                 direction[x][y] = "U";
             }
         }
@@ -166,4 +218,31 @@ void GET_LCS(vector<vector<string>> direction, vector<pair<int, int>> A, vector<
         GET_LCS(direction, A, answer, x, y - 1);
     else
         GET_LCS(direction, A, answer, x - 1, y);
+}
+
+int Remove_Duplicates(vector<int>& answerArray, vector<pair<int, int>> diameter, vector<pair<int, int>> strength)
+{
+    int currentDiameter = diameter[0].first;
+    int currentStrength = strength[0].first;
+    int duplicateCount = 0;
+    for (int i = 1; i < answerArray.size(); i++)
+    {
+        if (currentDiameter == diameter[i].first and currentStrength == strength[i].first)
+        {
+            answerArray[i] == -1;
+            duplicateCount++;
+        }
+        else
+        {
+            currentDiameter = diameter[i].first;
+            currentStrength = strength[i].first;
+        }
+    }
+
+    return duplicateCount;
+}
+
+int randNumber()
+{
+    return rand() % 10000;
 }
